ecd756f69e4a9e381b95633a71d645dfc784b260
Puneet <birdipuneet@gmail.com>
2020-04-15 16:30:18 -0400

    added JS to pull the values and display them in the DOM

--- airQuality.js
+++ airQuality.js
@@ -8,10 +8,23 @@ airQualityApp.getInfo = () => {
         method: 'GET',
         dataType: 'jsonp'
     }).then((result) => {
-        console.log(result)
+        requiredData = {
+            no2: result.data.iaqi.no2.v,
+            ozone: result.data.iaqi.o3.v,
+            partMatter: result.data.iaqi.pm25.v,
+            sulphurDiox: result.data.iaqi.so2.v,
+        }
+        airQualityApp.updateDOM(requiredData);
     })
 }
 
+airQualityApp.updateDOM = (cityData) => {
+    console.log(cityData);
+    $("#no2Value").html(`${cityData.no2}`);
+    $("#o3Value").html(`${cityData.ozone}`);
+    $("#pm25Value").html(`${cityData.partMatter}`);
+    $("#so2Value").html(`${cityData.sulphurDiox}`);
+}
 
 
 airQualityApp.init = () => {
